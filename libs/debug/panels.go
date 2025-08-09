27905314f0f3eb2da9b63ac385cae2d0f5021238
Thierry Geindre <thierry.geindre@bedrockstreaming.com>
2025-08-09 16:29:36 +0200

    Add debug info

--- libs/debug/panels.go
+++ libs/debug/panels.go
@@ -20,10 +20,21 @@ func DrawResolution(screen *ebiten.Image) {
 func DrawResolutionPos(screen *ebiten.Image, position ui.Position) {
 	ww, wh := ebiten.WindowSize()
 	mw, mh := ebiten.Monitor().Size()
-	ui.DrawPanel(screen, position, "Window: %d x %d\nMonitor: %d x %d", ww, wh, mw, mh)
+	ui.DrawPanel(screen, position, "Window %d x %d\nMonitor %dx%d\nBackend: %s", ww, wh, mw, mh, getBackend())
 }
 
 func DrawAll(screen *ebiten.Image) {
 	DrawFTPS(screen)
 	DrawResolution(screen)
+}
+
+var backend string
+
+func getBackend() string {
+	if backend == "" {
+		di := ebiten.DebugInfo{}
+		ebiten.ReadDebugInfo(&di)
+		backend = di.GraphicsLibrary.String()
+	}
+	return backend
 }
